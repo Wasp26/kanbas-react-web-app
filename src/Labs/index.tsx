6de67e8be168db46837bb39242729945e0fa7a0e
Sai Swapnil Aremanda <aremanda.s@northeastern.edu>
2024-05-22 19:50:23 +0530

    Adding details

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,17 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <p>
+        <b>First Name: </b>Sai Swapnil
+      </p>
+      <br />
+      <p>
+        <b>Last Name: </b>Aremanda
+      </p>
+      <p>
+        <b>Term: </b>
+      </p>
+      Summer 2024 (Full)
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
