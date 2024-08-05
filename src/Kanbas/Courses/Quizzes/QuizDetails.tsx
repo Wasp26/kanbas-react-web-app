9630f34db2f4a4e73cd75ed1956ac7c1cd466846
Sai Swapnil Aremanda <aremanda.s@northeastern.edu>
2024-08-05 23:23:36 +0530

    project commit for quiz list and details screen

---
+++ src/Kanbas/Courses/Quizzes/QuizDetails.tsx
@@ -0,0 +1,147 @@
+import { useEffect } from "react";
+import { CiEdit } from "react-icons/ci";
+import { useParams } from "react-router";
+import "./styles.css";
+import { Link } from "react-router-dom";
+export default function QuizDetails({
+  quizDetails,
+  fetchQuizDetails,
+}: {
+  quizDetails: any;
+  fetchQuizDetails: (qzid: string) => void;
+}) {
+  const { cid, qzid } = useParams();
+
+  useEffect(() => {
+    fetchQuizDetails(qzid as string);
+  }, []);
+
+  return (
+    <div id="wd-quiz-details">
+      <div className="w-100 justify-content-center align-self-center row mb-3 ps-5">
+        <div className="col-md-4">
+          <button className="btn btn-secondary me-3">Preview</button>
+          <Link to={`/Kanbas/Courses/${cid}/Quizzes/Editor/${qzid}`}>
+            <button className="btn btn-secondary">
+              <CiEdit className="me-1" />
+              Edit
+            </button>
+          </Link>
+        </div>
+      </div>
+      <hr />
+      <div id="wd-quiz-details-section">
+        <h1 className="mb-5">{quizDetails.title}</h1>
+        <div id="wd-details" className="ms-5 mt-3 mb-5 w-50">
+          <div className="row">
+            <div className="col-5">
+              <b>Quiz Type</b>
+            </div>
+            <div className="col-7">
+              <p>
+                {(quizDetails.quizType === "GRADEDQUIZ" && "Graded Quiz") ||
+                  (quizDetails.quizType === "PRACTICEQUIZ" &&
+                    "Practice Quiz") ||
+                  (quizDetails.quizType === "GRADEDSURVEY" &&
+                    "Graded Survey") ||
+                  (quizDetails.quizType === "UNGRADEDSURVEY" &&
+                    "Ungraded Survey")}
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-5">
+              <b>Points</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.points}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Assignment Group</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.group}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Shuffle Answers</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.shuffle ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Time Limit</b>
+            </div>
+            <div className="col-7">
+              <p>
+                {quizDetails.hasTimeLimit
+                  ? `${quizDetails.timeLimit} Minutes`
+                  : "None"}
+              </p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Multiple Attempts</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.multipleAttempt ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>One Question At a Time</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.oneQView ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Lock Questions after answering</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.lockAfterAttempt ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="row align-self-right">
+            <div className="col-5">
+              <b>Webcam Required</b>
+            </div>
+            <div className="col-7">
+              <p>{quizDetails.webcamReq ? "Yes" : "No"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <table className="table">
+        <tr>
+          <th scope="col">
+            <b>Due</b>
+          </th>
+          <th scope="col">
+            <b>For</b>
+          </th>
+          <th scope="col">
+            <b>Available From</b>
+          </th>
+          <th scope="col">
+            <b>Available Until</b>
+          </th>
+        </tr>
+        <tr>
+          <td>{quizDetails.dueDate.split("T")[0]}</td>
+          <td>Everyone</td>
+          <td>{quizDetails.availableFrom.split("T")[0]}</td>
+          <td>{quizDetails.availableUntil.split("T")[0]}</td>
+        </tr>
+      </table>
+    </div>
+  );
+  // ) <div>QuizDetails</div>;
+}
--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,63 +1,122 @@
-import { FaPlus } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import * as client from "./client";
-import { setQuizzes } from "./reducer";
-import { TiArrowSortedDown } from "react-icons/ti";
-import { useEffect } from "react";
-import { BsGripVertical } from "react-icons/bs";
+import { deleteQuiz, setQuizzes, updateQuiz } from "./reducer";
+import QuizDetails from "./QuizDetails";
+import QuizEditor from "./QuizEditor";
+import QuizList from "./QuizList";
+import { useState } from "react";
 
 export default function Quizzes() {
   const { cid } = useParams();
   const { isStaff } = useSelector((state: any) => state.accountReducer);
   const { quizzes } = useSelector((state: any) => state.quizzesReducer);
   const dispatch = useDispatch();
+  const dateToday = new Date();
 
   const fetchAllQuizzes = async () => {
     const quizzes = await client.fetchQuizzes(cid as string);
     dispatch(setQuizzes(quizzes));
   };
 
-  useEffect(() => {
-    fetchAllQuizzes();
-  }, []);
+  const updateQuizDetails = async (quiz: any) => {
+    await client.updateQuizDetails(cid as string, quiz);
+    dispatch(updateQuiz(quiz));
+  };
+
+  const deleteQuizDetails = async (quizId: string) => {
+    await client.deleteQuiz(cid as string, quizId);
+    dispatch(deleteQuiz(quizId));
+  };
+
+  const filterQuizzesByName = async (name: string) => {
+    if (name) {
+      const quizzes = await client.findQuizzesByPartialName(
+        cid as string,
+        name
+      );
+      dispatch(setQuizzes(quizzes));
+    } else {
+      fetchAllQuizzes();
+    }
+  };
+
+  const [quizDetails, setQuizDetails] = useState<any>({
+    courseId: cid,
+    numQuestions: 0,
+    published: false,
+    title: "New Quiz title",
+    description: "New quiz description",
+    quizType: "GRADEDQUIZ",
+    points: 0,
+    group: "QUIZ",
+    shuffle: true,
+    hasTimeLimit: true,
+    timeLimit: 20,
+    multipleAttempt: false,
+    showCorrect: false,
+    accessCode: "",
+    oneQView: true,
+    webcamReq: false,
+    lockAfterAttempt: false,
+    dueDate: "2024-07-01",
+    availableFrom: "2024-06-01",
+    availableUntil: "2024-07-08",
+  });
+
+  const fetchQuizDetails = async (qzid: string) => {
+    const quiz = await client.fetchQuizDetails(cid as string, qzid);
+    setQuizDetails(quiz);
+  };
+
+  const createQuizDetails = async () => {
+    const quiz = await client.createQuizDetails(cid as string, quizDetails);
+    setQuizDetails(quiz);
+    return quiz._id;
+  };
+
+  const saveQuizDetails = async () => {
+    await client.updateQuizDetails(cid as string, quizDetails);
+  };
 
   return (
     <div id="wd-quizzes">
-      {isStaff && (
-        <Link
-          to={`/Kanbas/Courses/${cid}/Quizzes/create`}
-          className="text-decoration-none btn btn-danger"
-        >
-          <FaPlus
-            className="position-relative me-2"
-            style={{ bottom: "1px" }}
-          />
-          Quiz
-        </Link>
-      )}
-      <hr />
-
-      <ul className="wd-assignments list-group rounded-0">
-        <li className="wd-asmnts-title list-item p-0">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <TiArrowSortedDown className="me-2 fs-3" />
-            Quizzes
-          </div>
-        </li>
-        {quizzes &&
-          quizzes.map((quiz: any) => (
-            <li className="wd-asmnt list-item p-3 ps-1">
-              <div className="row">
-                <div className="col-8">
-                  <Link to={`/Kanbas/Courses/${cid}/Assignments/${quiz._id}`}>
-                    {quiz.title}
-                  </Link>
-                </div>
-              </div>
-            </li>
-          ))}
-      </ul>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <QuizList
+              isStaff={isStaff}
+              quizzes={quizzes}
+              fetchAllQuizzes={fetchAllQuizzes}
+              updateQuizDetails={updateQuizDetails}
+              deleteQuizDetails={deleteQuizDetails}
+              filterQuizzesByName={filterQuizzesByName}
+            />
+          }
+        />
+        <Route
+          path="Editor/:qzid/*"
+          element={
+            <QuizEditor
+              quizDetails={quizDetails}
+              setQuizDetails={setQuizDetails}
+              createQuizDetails={createQuizDetails}
+              saveQuizDetails={saveQuizDetails}
+              fetchQuizDetails={fetchQuizDetails}
+            />
+          }
+        />
+        <Route
+          path=":qzid/Details"
+          element={
+            <QuizDetails
+              quizDetails={quizDetails}
+              fetchQuizDetails={fetchQuizDetails}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
